12b0d69d3ad624a0f28777713f6599f98a0bee06
Gopi Patel <ask2gopi@gmail.com>
2019-08-13 23:50:50 +1000

    seat arrangement with some css

--- src/components/Airplanes.js
+++ src/components/Airplanes.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './Airplanes.css'
 import axios from 'axios';
 
 const AIRPLANES_API = 'http://localhost:3000/airplanes.json';
@@ -25,11 +26,34 @@ class Airplanes extends Component {
   render() {
     return(
       <div>
+
       <h1> Airplane coming soon </h1>
       {this.state.airplanes.map(plane => (
+        <div>
         <p key={plane.id}>
-          {plane.name}
+          {plane.name} - {plane.rows} x {plane.columns}
         </p>
+        <table className="airplane-seats">
+
+          {[...Array(plane.rows).keys()].map(r => (
+            <tr>
+              <td class="noborder">{ String.fromCharCode(65 + r) }</td>
+              {Array(plane.columns).fill().map(c => (
+                <td> </td>
+              ))}
+            </tr>
+          ))}
+
+          <tr>
+            <td class="noborder"></td>
+            {[...Array(plane.columns).keys()].map(c => (
+              <td class="noborder"> {c+1} </td>
+            ))}
+          </tr>
+
+
+        </table>
+        </div>
       ))}
       </div>
     )
