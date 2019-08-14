9b448ca8e7145764f791347c610e545394c5506f
Ludovico Pinzari <ludovico.pinzari@gmail.com>
2019-08-15 09:59:23 +1000

    Add login and logout

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,8 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import Airplanes from './components/Airplanes';
 import Flights from './components/Flights';
 import Search from './components/Search';
+import LogIn from './components/LogIn';
+import LogOut from './components/LogOut';
 import App from './components/App';
 
 const Routes = (
@@ -11,6 +13,8 @@ const Routes = (
         <Route exact path="/airplanes" component={ Airplanes } />
         <Route exact path="/flights" component={ Flights } />
         <Route exact path="/search" component={ Search } />
+        <Route exact path="/login" component={ LogIn } />
+        <Route exact path="/logout" component={ LogOut } />
         <Route exact path="/" component={ App } />
       </div>
     </Router>
